30d64f3d88e2913efa743a6daf463a2a8e7a4139
Tim Roßkamp <timrosskamp97@gmail.com>
2018-05-28 19:42:54 +0200

    save

--- src/js/animation.js
+++ src/js/animation.js
@@ -2,7 +2,7 @@ const TL = anime.timeline({
     easing: 'easeInOutSine',
     loop: true,
     autoplay: false
-});
+})
 
 setTimeout(() => TL.play(), 700);
 
@@ -10,31 +10,60 @@ TL.add({
     targets: '.js-animation .js-progress',
     width: ['0%', '100%'],
     duration: 5000
-});
+})
+
+TL.add({
+    targets: '#frame-1 .js-img',
+    scale: [1, 1.2],
+    duration: 5000,
+    offset: "-=5000"
+})
 
 TL.add({
     targets: '#frame-1 .js-content',
     opacity: [0, 1],
     scale: [0.5, 1],
     offset: "-=5000"
-});
+})
 
 TL.add({
     targets: '#frame-1 .js-content line',
     strokeDashoffset: [anime.setDashoffset, 0],
     delay: (el, i) => i * 500,
     offset: "-=5000"
-});
+})
 
 TL.add({
     targets: '.js-animation .js-progress',
     width: ['0%', '100%'],
     duration: 5000
-});
+})
+
+TL.add({
+    targets: '#frame-2 .js-img',
+    scale: [1.2, 1],
+    translateX: ["5vw", 0],
+    duration: 5000,
+    offset: "-=5000"
+})
 
 TL.add({
     targets: '#frame-2',
     opacity: [0, 1],
     duration: 1500,
     offset: "-=5000"
-});
+})
+
+TL.add({
+    targets: '#frame-2 .js-content',
+    opacity: [0, 1],
+    scale: [0.5, 1],
+    offset: "-=4000"
+})
+
+TL.add({
+    targets: '#frame-2 .js-content line',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    delay: (el, i) => i * 500,
+    offset: "-=4000"
+})
